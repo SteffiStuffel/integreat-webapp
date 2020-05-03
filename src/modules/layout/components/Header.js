755076044fc1e5af5b10549202e5beb4c2b531c6
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2020-05-03 10:55:58 +0300

    adjust styles for the addition of the new library component

--- src/modules/layout/components/Header.js
+++ src/modules/layout/components/Header.js
@@ -32,6 +32,7 @@ const HeaderContainer = styled.header`
   box-shadow: 0 2px 5px -3px rgba(0, 0, 0, 0.2);
   background-color: ${props => props.theme.colors.backgroundAccentColor};
   user-select: none;
+  justify-content: center;
 
   & > div {
     display: flex;
@@ -94,6 +95,8 @@ const NavigationBar = styled(HeaderNavigationBar)`
   @media ${props => props.theme.dimensions.smallViewport} {
     flex: 1 0 100%;
     order: 3;
+    width: 100%;
+
   }
 `
 
